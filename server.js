d96094e389bac4bcb4a86be9f3511c2ca33b2877
Tega Erekata <tega23@yahoo.com>
2018-10-31 10:47:22 +0100

    chore : applied prettier

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
-const path = require('path');
+
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log('App listening on port', port);
 });
