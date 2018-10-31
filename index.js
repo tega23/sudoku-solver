75dbfe744edb98a819f4994a42f3febeb252964e
Tega Erekata <tega23@yahoo.com>
2018-10-31 08:58:52 +0100

    project setup

---
+++ index.js
@@ -0,0 +1,6 @@
+//each individual square is called a box
+//the complete rows.columns,3*3 squares are called units
+//given a particular box , its peers will be all other boxes that belong to a common unit
+//(those that belong to the same row, column, 3*3 square)
+
+
