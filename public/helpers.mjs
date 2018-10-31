f195f3779c6c53e4626e18c34013a5aee4c5bbf7
Tega Erekata <tega23@yahoo.com>
2018-10-31 10:59:32 +0100

    chore : grid value implementation

--- public/helpers.mjs
+++ public/helpers.mjs
@@ -7,3 +7,41 @@ export function cross(rows, cols) {
   }
   return result;
 }
+
+export function getRowUnits(rows, cols) {
+  let row_units = [];
+  for (let r of rows) {
+    row_units.push(cross(r, cols));
+  }
+  return row_units;
+}
+
+export function getColUnits(rows, cols) {
+  let col_units = [];
+  for (let c of cols) {
+    col_units.push(cross(rows, c));
+  }
+  return col_units;
+}
+
+export function getSquareUnits() {
+  let square_units = [];
+  for (let rs of ['ABC', 'DEF', 'GHI']) {
+    for (let cs of ['123', '456', '789']) {
+      square_units.push(cross(rs, cs));
+    }
+  }
+  return square_units;
+}
+
+export function grid_values(boxes, grid_string) {
+  let values = {};
+  for (let index in boxes) {
+    if (grid_string[index] == '.') {
+      values[boxes[index]] = '123456789';
+    } else {
+      values[boxes[index]] = grid_string[index];
+    }
+  }
+  return values;
+}
--- public/index.mjs
+++ public/index.mjs
@@ -3,9 +3,16 @@
 //given a particular box , its peers will be all other boxes that belong to a common unit
 //(those that belong to the same row, column, 3*3 square)
 'use strict';
-import { cross } from './helpers.mjs';
+import { cross, getColUnits, getRowUnits, getSquareUnits, grid_values } from './helpers.mjs';
 const rows = 'ABCDEFGHI';
 const cols = '123456789';
+const grid_string = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..';
 
 const boxes = cross(rows, cols);
-console.log(boxes);
+const row_units = getRowUnits(rows, cols);
+const col_units = getColUnits(rows, cols);
+const square_units = getSquareUnits();
+
+const grid_v = grid_values(boxes, grid_string);
+
+console.log(grid_v);
