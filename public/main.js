acb204620e18fd95f3bb4538135002bf1170d431
okekeobasi <okekeobasi@gmail.com>
2018-11-01 10:03:11 +0100

    fix : bug fix in the main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 // const grid_inputs = require('./sudoku_grids.mjs');
+/*
+ * Frontend Javascript
+*/
 
 const grid_inputs = [
   '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..',
